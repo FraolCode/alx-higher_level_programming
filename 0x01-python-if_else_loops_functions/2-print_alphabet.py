9b73941178bfb366bbd450486e226d960b2dc936
FraolCode <fraol.code@gmail.com>
2022-08-23 19:29:44 -0700

    print ASCII alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for letter_code in range(ord('a'), ord('z')+1):
-    letter = chr(letter_code)
-    print(letter, end="\n")
+for character in range(97, 123):
+    print("{:c}".format(character), end='')
