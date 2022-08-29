9db872d7c7c11ca4a606a072fa5d31b4accf417b
Fraol Girma <101295167+FraolCode@users.noreply.github.com>
2022-08-29 18:26:33 +0300

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,6 +5,4 @@ def no_c(my_string):
     for i in range(0, len(input_str)):
         if input_str[i] != 'c' and input_str[i] != 'C':
             result_str = result_str + input_str[i]
-   return(result_str)
-            
-        
+return(result_str)
