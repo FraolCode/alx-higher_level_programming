0b53b7b168d9522a3bafd0dae9d144350adbee37
Fraol Girma <101295167+FraolCode@users.noreply.github.com>
2022-08-29 18:27:40 +0300

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-result_str = ""
 def no_c(my_string):
-    for i in range(0, len(input_str)):
-        if input_str[i] != 'c' and input_str[i] != 'C':
-            result_str = result_str + input_str[i]
-return(result_str)
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
